b2eafd8ae174e19c3153c6fec5f56da68c430e70
srikvp <93551165+srikvp@users.noreply.github.com>
2024-12-07 10:42:47 +0530

    adding public subnet which was missed

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,9 +1,19 @@
-
+#refer readme file for reference documentation
 
 resource "aws_vpc" "main" {
   cidr_block = var.cidr #obtaining cidr info from corresponding env
   tags = {
     Name = "${var.env}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Name = "public-subnet"
   }
 }
 

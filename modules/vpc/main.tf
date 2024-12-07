e64dd891695dd9a869162d8dd2f5df0c8533eb05
srikvp <93551165+srikvp@users.noreply.github.com>
2024-12-07 10:17:28 +0530

    creating 4 subnets according to project requirement

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,3 +6,31 @@ resource "aws_vpc" "main" {
     Name = "${var.env}-vpc"
   }
 }
+
+resource "aws_subnet" "web" {
+  count             = length(var.web_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.web_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Name = "web-subnet"
+  }
+}
+resource "aws_subnet" "app" {
+  count             = length(var.app_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.app_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Name = "app-subnet"
+  }
+}
+resource "aws_subnet" "db" {
+  count = length(var.db_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.db_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Name = "db-subnet"
+  }
+}

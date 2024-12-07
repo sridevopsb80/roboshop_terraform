fa000b9f23b8f7c53187e517c26dc22f70f5b641
srikvp <93551165+srikvp@users.noreply.github.com>
2024-12-07 12:02:55 +0530

    defining route tables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -47,3 +47,34 @@ resource "aws_subnet" "db" {
   }
 }
 
+#Defining Route tables
+
+resource "aws_route_table" "public" {
+  count  = length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "public-rt-${split("-", var.availability_zones[count.index])[2]}"
+  }
+}
+
+resource "aws_route_table" "web" {
+  count  = length(var.web_subnets)
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "web-rt-${split("-", var.availability_zones[count.index])[2]}"
+  }
+}
+resource "aws_route_table" "app" {
+  count  = length(var.app_subnets)
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "app-rt-${split("-", var.availability_zones[count.index])[2]}"
+  }
+}
+resource "aws_route_table" "db" {
+  count  = length(var.db_subnets)
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "db-rt-${split("-", var.availability_zones[count.index])[2]}"
+  }
+}

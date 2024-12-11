1b1773c156e5314ba4239b1d5df843f822e7f42c
srikvp <93551165+srikvp@users.noreply.github.com>
2024-12-11 21:25:56 +0530

    renaming module to apps in main.tf since db does not need auto-scaling

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "vpc" {
   default_vpc_cidr   = var.vpc["default_vpc_cidr"]
 }
 
-module "ec2" {
+module "apps" {
   source        = "./modules/ec2"
   for_each      = var.ec2
   name          = each.key

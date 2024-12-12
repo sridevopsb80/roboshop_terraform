a4e7a55d3fe9de2c2b50caa57a809ccbed93d40c
srikvp <93551165+srikvp@users.noreply.github.com>
2024-12-12 19:00:30 +0530

    introducing dependencies to establish an order of execution

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "vpc" {
 
 module "apps" {
   source        = "./modules/ec2"
-  depends_on = [module.db]
+  depends_on = [module.db, module.vpc]
   for_each      = var.apps
   name          = each.key
   instance_type = each.value["instance_type"]
@@ -34,7 +34,7 @@ module "apps" {
 
 module "db" {
   source = "./modules/ec2"
-
+  depends_on = [module.vpc]
   for_each      = var.db
   name          = each.key
   instance_type = each.value["instance_type"]

2cf6849c96d74c267321ac7b34ce25a4c51d7be9
srikvp <93551165+srikvp@users.noreply.github.com>
2024-12-24 18:01:56 +0530

    define a new ansible role docker. define app_name

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -3,9 +3,8 @@
 dnf install docker -y
 growpart /dev/nvme0n1 4
 lvextend -r -L +10G /dev/mapper/RootVG-varVol
-
-#pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
-#ansible-pull -i localhost, -U https://github.com/sridevopsb80/roboshop-ansible main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
+pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/sridevopsb80/roboshop-ansible main.yml -e env=${env} -e role_name=docker -e app_name=${app_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
 
 #script to be run while instance is being launched
 #hvac is needed for vault_token

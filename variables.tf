dbd2e4a93d3b9ea4b321d82896090ea618610215
srikvp <93551165+srikvp@users.noreply.github.com>
2025-01-11 19:57:13 +0530

    cleaning up ec2 components

--- variables.tf
+++ variables.tf
@@ -1,8 +1,7 @@
 variable "vpc" {}
 variable "env" {}
-variable "apps" {}
 variable "bastion_nodes" {}
 variable "vault_token" {}
 variable "db" {}
 variable "zone_id" {}
-variable "load_balancers" {}
+

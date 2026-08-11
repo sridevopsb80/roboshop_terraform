652723a84b0864830c84345482a2b8b7f41d20d9
srikvp <93551165+srikvp@users.noreply.github.com>
2026-08-11 14:30:08 -0400

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-ENV="$1"
-ACTION="$2"
 
 # $0-run.sh, $1-dev/prod, $2- apply/destroy
 # 2 arguments are expected for env and action. if it is not provided
 # $# - counts the number of arguments provided. 
 # if the cli argument input is not 2, then print the below.
 
+set -e
+
+echo " Execution command: run.sh <dev or prod> <apply or destroy> "
 
 if [ $# -ne 2 ]; then
-  echo "$0 env(dev|prod) action(apply|destroy)"
+  echo "Use $0 env(dev|prod) action(apply|destroy)"
   exit 1
 fi
+
+ENV="$1"
+ACTION="$2"
 
 if [[ "$ENV" != "dev" && "$ENV" != "prod" ]]; then
     echo "Environment must be dev or prod"
@@ -26,5 +30,7 @@ fi
 git pull
 rm -rf .terraform
 terraform init -backend-config=env-"${ENV}"/state.tfvars
-terraform "$ACTION" -var-file=env-"${ENV}"/main.tfvars -auto-approve
+terraform "$ACTION" \
+  -var-file=env-"${ENV}"/main.tfvars \
+  -auto-approve
 
